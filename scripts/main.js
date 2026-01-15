a8192f0b4841654eaa4629b106d4e60cfe9c5bec
dany-quint <danyquintana60@gmail.com>
2026-01-15 02:03:14 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Botón comenzar camino
   const startBtn = document.getElementById("startBtn");
-  const inicioSection = document.getElementById("Capítulo-1");
+  const inicioSection = document.getElementById("capitulo-1");
 
   if (startBtn && inicioSection) {
     startBtn.addEventListener("click", () => {
